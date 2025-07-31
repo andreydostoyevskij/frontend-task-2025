e59e415ce025343785ef7fcb6db98f04ba166ac0
andreydostoyevskij <andreydostoyevskij@gmail.com>
2025-07-31 18:07:36 +0300

    install heroui and some configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 // tailwind.config.js
-const {heroui} = require("@heroui/theme");
+const { heroui } = require("@heroui/theme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./node_modules/@heroui/theme/dist/components/(button|ripple|spinner).js",
-  ],
+  content: ["./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
